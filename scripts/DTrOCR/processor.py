744c8f6f346f6dac4c3ea14688b873da93531c10
Yalanskyi Roman <keito.eternal@gmail.com>
2024-09-22 22:38:53 +0300

    adjusted config to use GPT2 instead of T5

--- scripts/DTrOCR/processor.py
+++ scripts/DTrOCR/processor.py
@@ -1,14 +1,14 @@
-from transformers import T5TokenizerFast, AutoImageProcessor
+from transformers import GPT2Tokenizer, AutoImageProcessor
 
 from PIL import Image
 from typing import List, Union
+
 from config import DTrOCRConfig
 from data import DTrOCRProcessorOutput
 
 
 class DTrOCRProcessor:
-    def __init__(self, config: DTrOCRConfig, add_bos_token: bool = False,
-                 add_eos_token: bool = False):
+    def __init__(self, config: DTrOCRConfig, add_bos_token: bool = False, add_eos_token: bool = False):
         self.vit_processor = AutoImageProcessor.from_pretrained(
             config.vit_hf_model,
             size={
@@ -17,19 +17,20 @@ class DTrOCRProcessor:
             },
             use_fast=True
         )
-
-        self.tokenizer = T5TokenizerFast.from_pretrained(
-            config.t5_model,
+        self.tokeniser = GPT2Tokenizer.from_pretrained(
+            config.gpt2_hf_model,
+            add_bos_token=add_bos_token,
             model_max_length=config.max_position_embeddings - int(
-                (config.image_size[0] / config.patch_size[0]) *
-                (config.image_size[1] / config.patch_size[1])
+                (config.image_size[0] / config.patch_size[0]) * (config.image_size[1] / config.patch_size[1])
             )
         )
+        self.tokeniser.pad_token = self.tokeniser.bos_token
+        self.tokeniser.add_eos_token = add_eos_token
 
-        self.add_bos_token = add_bos_token
-        self.add_eos_token = add_eos_token
-
-        self.tokenizer.pad_token = self.tokenizer.eos_token
+        # Bind a new method to gpt2_tokeniser
+        self.tokeniser.build_inputs_with_special_tokens = modified_build_inputs_with_special_tokens.__get__(
+            self.tokeniser
+        )
 
     def __call__(
         self,
@@ -41,26 +42,36 @@ class DTrOCRProcessor:
         *args,
         **kwargs
     ) -> DTrOCRProcessorOutput:
-        text_inputs = self.tokenizer(
-            texts, padding=padding, add_special_tokens=True,
-            *args, **kwargs
+        text_inputs = self.tokeniser(
+            texts, padding=padding, *args, **kwargs
         ) if texts is not None else None
 
         image_inputs = self.vit_processor(
-            images, input_data_format=input_data_format,
-            *args, **kwargs
+            images, input_data_format=input_data_format, *args, **kwargs
         ) if images is not None else None
 
         return DTrOCRProcessorOutput(
-            pixel_values=image_inputs["pixel_values"] 
-            if images is not None else None,
-            
-            input_ids=text_inputs['input_ids']
-            if texts is not None else None,
-            
-            attention_mask=text_inputs['attention_mask']
-            if texts is not None else None,
-            
-            labels=text_inputs['input_ids']
-            if texts is not None and return_labels else None
+            pixel_values=image_inputs["pixel_values"] if images is not None else None,
+            input_ids=text_inputs['input_ids'] if texts is not None else None,
+            attention_mask=text_inputs['attention_mask'] if texts is not None else None,
+            labels=text_inputs['input_ids'] if texts is not None and return_labels else None
         )
+
+
+def modified_build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
+    if self.add_bos_token:
+        bos_token_ids = [self.bos_token_id]
+    else:
+        bos_token_ids = []
+
+    if self.add_eos_token:
+        eos_token_ids = [self.eos_token_id]
+    else:
+        eos_token_ids = []
+
+    output = bos_token_ids + token_ids_0 + eos_token_ids
+
+    if token_ids_1 is None:
+        return output
+
+    return output + bos_token_ids + token_ids_1
