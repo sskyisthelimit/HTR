3489c89d1a21235062ec63729d114f44eba03ea7
Yalanskyi Roman <keito.eternal@gmail.com>
2024-10-21 16:09:39 +0300

    detector class fix - Fixed typo - self.yolo_model call instead of self.model - Fixed config overriding - Additional error-throwing for cuda

--- models/detection/detector.py
+++ models/detection/detector.py
@@ -1,14 +1,26 @@
 from ultralytics import YOLO
+import torch
 
 
 class Detector:
-    def __init__(self, model_pt_path):
-        self.model = YOLO(model_pt_path)
-
-    def generate_results(self, np_image,
-                         image_size, device, max_det):
+    def __init__(self, model_pt_path, yaml_config_path):
+        if not torch.cuda.is_available():
+            raise ValueError(
+                "CUDA is not available, detector WON'T BE initialized")
         
-        return self.yolo_model.predict(
-            source=np_image, imgsz=image_size,
-            device=device, max_det=max_det
-            )
+        try:
+            self.model = YOLO(model_pt_path, stream=True)
+            self.model.overrides['data'] = yaml_config_path
+        except Exception as e:
+            print(f"Error loading model: {e}")
+
+    def generate_results(self, np_image, image_size, device,
+                         max_det, **kwargs):
+
+        return self.model.predict(
+            source=np_image,
+            imgsz=image_size,
+            device=device,
+            max_det=max_det,
+            **kwargs
+        )
