c62e69c1d1cff1a4c0de5dad3501ffae08e8531d
Yalanskyi Roman <keito.eternal@gmail.com>
2024-11-02 17:51:48 +0200

    terraform infrastructure init

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+resource "aws_ecr_repository" "htr_api_repo" {
+  name = "htr-api-repo"
+}
+
+data "aws_iam_policy_document" "ecr_access_policy_doc" {
+  statement {
+    sid    = "AllowECRAccess"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ecr_access_policy" {
+  name   = "ECRAccessPolicy"
+  policy = data.aws_iam_policy_document.ecr_access_policy_doc.json
+}
+
+data "aws_iam_policy_document" "ec2_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name               = "htr-api-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ecr_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ecr_access_policy.arn
+}
+
